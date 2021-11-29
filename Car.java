7ed0fa0406fa7bdbb26e2f4f48898a2fcda3ebc2
Lara Zughlul <91340250+Lara-Zughlul@users.noreply.github.com>
2021-11-29 20:42:31 +0300

    Update Car.java

--- Car.java
+++ Car.java
@@ -1,1 +1,92 @@
+package myrental;
 
+public class Car {
+    
+    private String company, name, year, color, plateNum, type;
+    private boolean availability;
+    private double price;
+
+    public Car() {
+    }
+
+    public Car(String company, String name, String year, String color, String plateNum, String type, boolean availability, double price) {
+        this.company = company;
+        this.name = name;
+        this.year = year;
+        this.color = color;
+        this.plateNum = plateNum;
+        this.type = type;
+        this.availability = availability;
+        this.price = price;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getPlateNum() {
+        return plateNum;
+    }
+
+    public void setPlateNum(String plateNum) {
+        this.plateNum = plateNum;
+    }
+
+    public boolean isAvailability() {
+        return availability;
+    }
+
+    public void setAvailability(boolean availability) {
+        this.availability = availability;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" + "company=" + company + ", name=" + name + ", year=" + year + ", color=" + color + ", plateNum=" + plateNum + ", type=" + type + ", availability=" + availability + ", price=" + price + '}';
+    }
+    
+} //End of class
