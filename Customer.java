1d5e6cca32211ba4b1e2bcca001be454f0b358c7
Ghalamg <91337477+Ghalamg@users.noreply.github.com>
2021-11-29 20:59:36 +0300

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,49 +1,96 @@
 package myrental;
 
 import java.util.Date;
-import static myrental.MyRental.input1;
 import static myrental.MyRental.customer;
+import static myrental.MyRental.input1;
 
 public class Customer  extends User {
     
-    private String region,city,district,street;
-    private int buildingNo, postNo, CVV,ID;
-    private long cardNum;
-    private Date expireDate;
+    private String fName,lName,username,password,email,cardNum;
+            //,region,city,district,street;
+    private int CVV,ID;
+           // buildingNo, postNo, ;
+    
+    private String expireDate;
     private Rental rental;
 
     public Customer() {
     }
-    
-    
 
-    public Customer(int ID, String region, String city, String district, String street, int buildingNo, int postNo, int CVV, long cardNum, Date expireDate) {
-        this.ID = ID;
-        this.region = region;
-        this.city = city;
-        this.district = district;
-        this.street = street;
-        this.buildingNo = buildingNo;
-        this.postNo = postNo;
-        this.CVV = CVV;
-        this.cardNum = cardNum;
-        this.expireDate = expireDate;
+    public Customer(String username, String password) {
+        this.username = username;
+        this.password = password;
     }
 
-    public Customer(int ID, String region, String city, String district, String street, int buildingNo, int postNo, int CVV, long cardNum, Date expireDate, String Fname, String Lname, String UserName, String password, String Email) {
-        super(Fname, Lname, UserName, password, Email);
-        this.ID = ID;
-        this.region = region;
-        this.city = city;
-        this.district = district;
-        this.street = street;
-        this.buildingNo = buildingNo;
-        this.postNo = postNo;
+    public Customer(String fName, String lName, String username, String password, String email, int CVV, int ID, String cardNum, String expireDate, Rental rental) {
+        this.fName = fName;
+        this.lName = lName;
+        this.username = username;
+        this.password = password;
+        this.email = email;
         this.CVV = CVV;
+        this.ID = ID;
         this.cardNum = cardNum;
         this.expireDate = expireDate;
+        this.rental = rental;
     }
 
+    public Customer(String fName, String lName, String username, String password, String email) {
+        this.fName = fName;
+        this.lName = lName;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    
+    
+
+   
     public Rental getRental() {
         return rental;
     }
@@ -58,7 +105,7 @@ public class Customer  extends User {
         return ID;
     }
 
-    public String getRegion() {
+    /*public String getRegion() {
         return region;
     }
 
@@ -80,24 +127,24 @@ public class Customer  extends User {
 
     public int getPostNo() {
         return postNo;
-    }
+    }*/
 
     public int getCVV() {
         return CVV;
     }
 
-    public long getCardNum() {
+    public String getCardNum() {
         return cardNum;
     }
 
-    public Date getExpireDate() {
+    public String getExpireDate() {
         return expireDate;
     }
 
     public void setID(int ID) {
         this.ID = ID;
     }
-
+/*
     public void setRegion(String region) {
         this.region = region;
     }
@@ -120,27 +167,27 @@ public class Customer  extends User {
 
     public void setPostNo(int postNo) {
         this.postNo = postNo;
-    }
+    }*/
 
     public void setCVV(int CVV) {
         this.CVV = CVV;
     }
 
-    public void setCardNum(long cardNum) {
+    public void setCardNum(String cardNum) {
         this.cardNum = cardNum;
     }
 
-    public void setExpireDate(Date expireDate) {
+    public void setExpireDate(String expireDate) {
         this.expireDate = expireDate;
     }
     
-    public void displayInfo(){
+   /* public void displayInfo(){
         
     }
     
     public void editProfile(){
         
-    }
+    }*/
     
     public boolean addPaymentInfo(long cardNum, Date expireDate, int CVV){
         return true;
@@ -149,7 +196,9 @@ public class Customer  extends User {
     public void viewRentalHistory(){
         
     }
-    public static Customer Signup(){
+    
+    public  Customer Signup(){
+      
         String Fname,Lname,Username,password,Email;
         System.out.println("enter your first and last name");
         Fname=input1.next();
@@ -161,13 +210,87 @@ public class Customer  extends User {
         System.out.println("enter email");
         Email=input1.next();
          
-        Customer newUser=new Customer();
+        Customer newUser=new Customer(Fname,Lname,Username,password,Email);
+        System.out.println("sign-up succesful ");
+        login();
         return newUser;
    }
+    
+    //read username and password from user and send to search method
+    public  void login() {
+        
+        String username;
+        String password;
+        Customer currentCustomer = new Customer( this.username, this.password);
+        System.out.println("enter your username");
+        username = input1.next();
+        System.out.println("enter your password");
+        password = input1.next();
 
+        if (searchCustomer(username, password) == true) {
+            System.out.println("login succes \n");
+
+        } else if (searchCustomer(username, password) == false) {
+            System.out.println("username or password is incorrect, try again");
+            login();
+
+        }  //End of if/else if/else
+
+    } //End of login method
+
+    public  boolean searchCustomer(String username, String password) {
+        //search for user in database
+        for (int i = 0; i < customer.length; i++) {
+
+            //Return true if username & password of user are both correct
+            if (customer[i].getUsername().equals(username) && customer[i].getPassword().equals(password)) {
+                //custID = i;
+                return true;
+
+                // Return false if username is correct but the password is wrong
+            } else if (!customer[i].getPassword().equals(password) && customer[i].getUsername().equals(username)) {
+                return false;
+
+            } //End of if/else if
+        } //End of for loop
+        return false;
+    } //End of searchUser method
+
+    public void makePayment(int Id){
+        //cardnum, cvv , exp date,
+        
+         System.out.println("----------------------------------------------------");
+        System.out.println("    Step 6: Payment ");
+        System.out.println();
+        System.out.println("Select payment method: \n"
+        + "1- Credit card \n"
+        + "2- Apple pay (not implemented) \n"
+        + "3- Cash (not implemented)");
+        int choice = input1.nextInt();
+        if(choice==1){
+            System.out.println();
+        System.out.println("Enter card number: (no spaces between numbers)");
+        String cardnumber = input1.next();
+        System.out.println("Enter card expiry date (MM/YY): ");
+        String expirydate = input1.next();
+        System.out.println("Enter CVV number (behind the card): ");
+        int cvv= input1.nextInt();
+       customer[Id].setCardNum(cardnumber);
+       customer[Id].setExpireDate(expirydate);
+       customer[Id].setCVV(cvv);
+        }else{
+            System.out.println("This method is not available yet! Select different method");
+            makePayment(Id);
+        }
+         System.out.println("----------------------------------------------------");
+      
+       
+        
+     }
+ 
     @Override
     public String toString() {
-        return "Customer{" + "ID=" + ID + ", region=" + region + ", city=" + city + ", district=" + district + ", street=" + street + ", buildingNo=" + buildingNo + ", postNo=" + postNo + ", CVV=" + CVV + ", cardNum=" + cardNum + ", expireDate=" + expireDate + '}';
+        return "Customer{ id:" +ID+ ", first name: " + fName + ", last name: " + lName + ", username: " + username + ", password: " + password + ", email: " + email + '}';
     }
     
 } //End of class
