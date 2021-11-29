149b63a3e38dc788ab2b29f6c0adc1fa5a991e44
Lara Zughlul <91340250+Lara-Zughlul@users.noreply.github.com>
2021-11-29 20:43:51 +0300

    Update MyRental.java

--- MyRental.java
+++ MyRental.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package myrental;
 
 import java.io.File;
@@ -10,228 +5,313 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-/**
- *
- * @author danii
- */
+
+//iteration1 : make rental  Done
+//iteration 2: edit rental
+//iteration 3: payment
 public class MyRental {
 
-    static User user[] = new User[4];//array of users with 4 slots
-    static Scanner input1 = new Scanner(System.in);//scanner to read from user
+    //array of users with 4 slots
+    static Customer customer[] = new Customer[4];
+    //array of cars
     static Car car[] = new Car[3];
+    //scanner to read from user
+    static Scanner input1 = new Scanner(System.in);
+    static int custID, carNum;
+    static String start, end, region1, city1, branch1;
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) throws FileNotFoundException {
 
-        File users = new File("usersDatabase.txt");//file with users in database
-        if (!users.exists()) {
-            //check if the files exist.
+        //File with users in database
+        File customers = new File("usersDatabase.txt");
+        if (!customers.exists()) {
+            //check if the file exists.
             System.out.println("the file does not exist ");
             System.exit(0);
-        }
-        Scanner input = new Scanner(users);//scanner to read from file
-        Database(users, input);//database method call(read 3 users from file and store in users aray of objects)
+        } //End of if
 
-        //-------------------------------------------
-        //database for cars
-        File cars = new File("carsDatabase.txt");//file with users in database
+        //File with cars in database
+        File cars = new File("carsDatabase.txt");
         if (!cars.exists()) {
-            //check if the files exist.
+            //check if the file exists.
             System.out.println("the file does not exist ");
             System.exit(0);
-        }
-        Scanner input2 = new Scanner(cars);//scanner to read from file
-        Database2(cars, input2);
-        //log in choices
+        } //End of if
+
+        //scanner to read from file
+        Scanner input = new Scanner(customers);
+        //usersDatabase method call(read 3 users from file and store in users array of objects)
+        usersDatabase(customers, input);
+
+        //scanner to read from file
+        Scanner input2 = new Scanner(cars);
+        //carrsDatabase method call(read 3 cars from file and store in cars array of objects)
+        carsDatabase(cars, input2);
+
+        //login choices
         System.out.println("              MYRENTAL              " + "\n enter 1 to log in and 2 to sign up");
         int choice = input1.nextInt();
+
         if (choice == 1) {
-            //if the user choose to login
+            //if the user chooses to login
             login();
         } else if (choice == 2) {
-            user[4] = signUp();
-            //add new user to the 4th empty slot
-        }
+            //if the user chooses to suign up, add new user to the 4th empty slot
+            customer[4] = signUp();
+        } //End of if/else
 
-        makeRental();
+        //--------------Home Menu--------------
+        int service;
 
-        input.close();
-
-    }
-
-    public static void login() {//read username and password from user and send to search method
-        User currentUser = new User();
-        String username;
-        String password;
-        int Service;
-        int Region;
-        int City;
-        int branch;
-
-        System.out.println("enter your username");
-        username = input1.next();
-        System.out.println("enter your password");
-        password = input1.next();
-
-        if (searchUser(username, password) == true) {
-            System.out.println("login succes");
-        } else if (searchUser(username, password) == false) {
-            System.out.println("wrong password,try again");
-            login();
-        } else {
-            System.out.println("user does not exist,try again");
-            login();
-
-        }
-        //---------------------------------------------------
-        //choose service
-        System.out.println("please enter your choice :");
+        //Choose service
+        System.out.println("Please enter your choice :");
         System.out.println("1-view profile\n"
                 + "2-make rental\n"
                 + "3-edit current rentals\n"
                 + "4-rental history");
-        Service = input1.nextInt();
+        service = input1.nextInt();
         System.out.println();
 
-        while (Service > 4) {
+        //If user enters a wrong choice (number), then repeat
+        while (service > 4) {
             System.out.println("wrong choice, choose again");
             System.out.println("1-view profile\n"
                     + "2-make rental\n"
                     + "3-edit current rentals\n"
                     + "4-rental history");
-            Service = input1.nextInt();
+            service = input1.nextInt();
             System.out.println();
+        } //End of while loop
 
+        //Make rental
+        if (service == 2) {
+            makeRental(service);
+            selectDate();
+            chooseCar();
+            Rental rental = new Rental(start, end, region1, branch1, city1, car[carNum], custID);
+            customer[custID].setRental(rental);
+        } //End of if
+
+        input.close();
+        input2.close();
+
+    } //End of main method
+
+    //read username and password from user and send to search method
+    public static void login() {
+        Customer currentCustomer = new Customer();
+        String username;
+        String password;
+        System.out.println("enter your username");
+        username = input1.next();
+        System.out.println("enter your password");
+        password = input1.next();
+
+        if (searchCustomer(username, password) == true) {
+            System.out.println("login succes \n");
+
+        } else if (searchCustomer(username, password) == false) {
+            System.out.println("username or password is incorrect, try again");
+            login();
+
+        }  //End of if/else if/else
+
+    } //End of login method
+
+    public static boolean searchCustomer(String username, String password) {
+        //search for user in database
+        for (int i = 0; i < customer.length - 1; i++) {
+
+            //Return true if username & password of user are both correct
+            if (customer[i].getUserName().equals(username) && customer[i].getPassword().equals(password)) {
+                custID = i;
+                return true;
+
+                // Return false if username is correct but the password is wrong
+            } else if (!customer[i].getPassword().equals(password) && customer[i].getUserName().equals(username)) {
+                return false;
+
+            } //End of if/else if
+        } //End of for loop
+        return false;
+    } //End of searchUser method
+
+    public static Customer signUp() {
+        Customer newCustomer = Customer.Signup();
+        System.out.println("sign-up succesful ");
+        login();//go to login page after sign up
+        return newCustomer;
+
+    } //End of signUp method
+
+    public static void usersDatabase(File customers, Scanner input) {
+
+        //read 3 users information from file and store in users array of object
+        int i = 0;
+        while (input.hasNext()) {
+            customer[i] = new Customer();
+            customer[i].setID(i);
+            customer[i].setFname(input.next());
+            customer[i].setLname(input.next());
+            customer[i].setUserName(input.next());
+            customer[i].setPassword(input.next());
+            customer[i].setEmail(input.next());
+
+            i++;
+
+        } //End of while loop
+
+        //input.close();
+    } //End of Database method
+
+    public static void carsDatabase(File cars, Scanner input) {
+
+        //read 3 cars information from file and store in cars array of object
+        int i = 0;
+        while (input.hasNext()) {
+            car[i] = new Car();
+            car[i].setCompany(input.next());
+            car[i].setName(input.next());
+            car[i].setYear(input.next());
+            car[i].setType(input.next());
+            car[i].setColor(input.next());
+            car[i].setPlateNum(input.next());
+            car[i].setAvailability(input.nextBoolean());
+            car[i].setPrice(input.nextDouble());
+
+            i++;
+
+        } //End of while loop
+
+        //input.close();
+    } //End of Database method
+
+    public static void makeRental(int service) {
+
+        int region, city, branch;
+
+        //Choose region
+        System.out.println("Choose the region:\n"
+                + "1-Makkah Region\n"
+                + "2-Riyadh Region");
+        region = input1.nextInt();
+        if (region == 1) {
+            region1 = "Makkah";
+        } else {
+            region1 = "Riyadh";
         }
-        //---------------------------
-        //make rental--> choose region
-        if (Service == 2) {
-            System.out.println("Choose the region:\n"
-                    + "1-Makkah Region\n"
-                    + "2-Riyadh Region");
-        }
-        Region = input1.nextInt();
         System.out.println();
 
-        while (Service > 4) {
+        //If user enters a wrong choice (number), then repeat
+        while (region > 2) {
             System.out.println("wrong choice, choose again");
             System.out.println("Choose the region:\n"
                     + "1-Makkah Region\n"
                     + "2-Riyadh Region");
-            Region = input1.nextInt();
+            region = input1.nextInt();
             System.out.println();
+        } //End of while loop
 
-        }
-        //--------------------------
-        //make rental--> choose city and branch
-        switch (Region) {
-            case 1:
+        //Choose city & branch
+        switch (region) {
+            case 1: {
+                //Choose city in Makkah Region
                 System.out.println("Choose the city:\n"
                         + "1-Makkah\n"
                         + "2-Jeddah");
-                City = input1.nextInt();
+                city = input1.nextInt();
                 System.out.println();
 
-                switch (City) {
-                    case 1:
-                        System.out.println("Enter branch number:\n"
-                                + "1-Albawadi branch\n"
-                                + "2-Alharamain branch");
-                        branch = input1.nextInt();
-                        System.out.println();
+                if (city == 1) {
+                    //Choose branch in Makkah
+                    System.out.println("Enter branch number:\n"
+                            + "1-Albawadi branch\n"
+                            + "2-Alharamain branch");
+                    branch = input1.nextInt();
+                    System.out.println();
+                    city1 = "Makkah";
+                    if (branch == 1) {
+                        branch1 = "Albawadi branch";
+                    } else {
+                        branch1 = "Alharamain branch";
+                    }
 
-                        break;
-                    case 2:
-                        System.out.println("Enter branch number:\n"
-                                + "1-Alfaihaa branch\n"
-                                + "2-Alnaseem branch");
-                        branch = input1.nextInt();
-                        System.out.println();
+                } else if (city == 2) {
+                    //Choose branch in Jeddah
+                    System.out.println("Enter branch number:\n"
+                            + "1-Alfaihaa branch\n"
+                            + "2-Alnaseem branch");
+                    branch = input1.nextInt();
+                    System.out.println();
+                    city1 = "Jeddah";
+                    if (branch == 1) {
+                        branch1 = "Alfaihaa branch";
+                    } else {
+                        branch1 = "Alnaseem branch";
+                    }
+                } //End of if/else
 
-                        break;
-                }
-                break;
-            case 2:
+            }
+            break; //End of case 1 
+
+            case 2: {
+                //Choose city in Riyadh Region
                 System.out.println("Choose the city:\n"
                         + "1-Riyadh");
-                City = input1.nextInt();
+                city = input1.nextInt();
                 System.out.println();
-                switch (City) {
-                    case 1:
-                        System.out.println("Enter branch number:\n"
-                                + "1-Alshaalan branch\n"
-                                + "2-Alwadi branch");
-                        branch = input1.nextInt();
-                        System.out.println();
+                city1 = "Riyadh";
 
-                        break;
-
-                }
-                break;
-        }
-    }
-
-    public static boolean searchUser(String username, String password) {
-        //search for user in database and return 1 if found and 2 if password wrong and o if not exist
-        for (int i = 0; i < user.length - 1; i++) {
-            //&&user[i].getPassword()==password
-            if (user[i].getUserName().equals(username) && user[i].getPassword().equals(password)) {
-                return true;
-            } else if (!user[i].getPassword().equals(password) && user[i].getUserName().equals(username)) {
-                return false;
+                if (city == 1) {
+                    //Choose branch in Riyadh
+                    System.out.println("Enter branch number:\n"
+                            + "1-Alshaalan branch\n"
+                            + "2-Alwadi branch");
+                    branch = input1.nextInt();
+                    System.out.println();
+                    if (branch == 1) {
+                        branch1 = "Alshaalan branch";
+                    } else {
+                        branch1 = "Alwadi branch";
+                    }
+                } //End of if
             }
-        }
-        return true;
+            break; //End of case 2
+
+            default: {
+
+            }
+            break; //End of default case (error)
+
+        } //End of switch case
+
+    } //End of makeRental method
+
+    public static void selectDate() {
+        System.out.println("Enter starting date: (DD/MM/YYYY)");
+        start = input1.next();
+        System.out.println("Enter ending date: (DD/MM/YYYY)");
+        end = input1.next();
+        System.out.println();
     }
 
-    public static User signUp() {//
-        User newUser = User.Signup();
-        System.out.println("sign-up succesful ");
-        login();//go to login page after signup
-        return newUser;
+    public static void chooseCar() {
 
-    }
+        for (int i = 0; i < car.length; i++) {
 
-    public static void Database(File users, Scanner input) {
-        //read 3 users information from file and store in users array of object
+            System.out.println("Choose a car: \n"
+                    + (i + 1) + "- \n"
+                    + "Model: " + car[i].getCompany() + " " + car[i].getName() + " " + car[i].getYear() + "\n"
+                    + "Type: " + car[i].getType() + "\n"
+                    + "Color: " + car[i].getColor() + "\n"
+                    + "Price: " + car[i].getPrice() + "$"+"\n");
+        } //End of for loop
+        int choice = input1.nextInt();
+        carNum = choice;
 
-        int i = 0;
-        while (input.hasNext()) {
-            user[i] = new User();
-            user[i].setFname(input.next());
-            user[i].setLname(input.next());
-            user[i].setUserName(input.next());
-            user[i].setPassword(input.next());
-            user[i].setEmail(input.next());
+    } //End of chooseCar method
 
-            i++;
-
-        }
-
-        input.close();
-
-    }
-
-    public static void Database2(File cars, Scanner input2) {
-        int i = 0;
-        while (input2.hasNext()) {
-              car[i]=new Car();
-            car[i].setCompany(input2.next());
-            car[i].setName(input2.next());
-            car[i].setYear(input2.next());
-            car[i].setType(input2.next());
-            car[i].setColor(input2.next());
-            car[i].setPlateNum(input2.next());
-            car[i].setAvailability(input2.nextBoolean());
-            car[i].setPrice(input2.nextDouble());
-
-        }
-        input2.close();
-    }
-
-    public static void makeRental() {
-
-    }
-}
+}  //End of class
