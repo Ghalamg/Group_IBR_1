95dd1fdeaff05ecbcc17438989a2c81dae630dce
Ghalamg <91337477+Ghalamg@users.noreply.github.com>
2021-11-29 21:00:57 +0300

    Update Rental.java

--- Rental.java
+++ Rental.java
@@ -1,112 +1,315 @@
 package myrental;
 
-import java.util.Date;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
-public class Rental {
+
+//iteration1 : make rental 
+//iteration 2: make payment
+//iteration 3: select services
+
+public class MyRental {
+
+    //array of users with 4 slots
+    static Customer customer[] = new Customer[4];
+    //array of cars with 3 slots
+    static Car car[] = new Car[3];
+    //scanner to read from user
+    static Scanner input1 = new Scanner(System.in);
+    //attributes
+    static int custID, carNum;
+    static String start, end, region1, city1, branch1;
+    static Customer cust=null;
+   static services service = new services();
     
-    private String Startingdate, endingDate;
-    private String region, branch,city;
-    private Car car;
-    private int custID;
-    private Manager manager;
+    /**
+     * @param args the command line arguments
+     * @throws java.io.FileNotFoundException
+     */
+    public static void main(String[] args) throws FileNotFoundException {
 
-    public Rental(String Startingdate, String endingDate, String region,String branch, String city, Car car, int custID) {
-        this.Startingdate = Startingdate;
-        this.endingDate = endingDate;
-        this.region = region;
-        this.branch=branch;
-        this.city = city;
-        this.car = car;
-        this.custID = custID;
-    }
-    
-    
-    public Rental(String Startingdate, String endingDate, String region, String city, Car car, int custID, Manager manager) {
-        this.Startingdate = Startingdate;
-        this.endingDate = endingDate;
-        this.region = region;
-        this.city = city;
-        this.car = car;
-        this.custID = custID;
-        this.manager = manager;
-    }
+        //File with users in database
+        File customers = new File("usersDatabase.txt");
+        if (!customers.exists()) {
+            //check if the file exists.
+            System.out.println("the file does not exist ");
+            System.exit(0);
+        } //End of if
 
-    public void setStartingdate(String Startingdate) {
-        this.Startingdate = Startingdate;
-    }
+        //File with cars in database
+        File cars = new File("carsDatabase.txt");
+        if (!cars.exists()) {
+            //check if the file exists.
+            System.out.println("the file does not exist ");
+            System.exit(0);
+        } //End of if
 
-    public void setEndingDate(String endingDate) {
-        this.endingDate = endingDate;
-    }
+        //scanner to read from file
+        Scanner input = new Scanner(customers);
+        //usersDatabase method call(read 3 users from file and store in users array of objects)
+        usersDatabase(customers, input);
 
-    public void setRegion(String region) {
-        this.region = region;
-    }
+        //scanner to read from file
+        Scanner input2 = new Scanner(cars);
+        //carrsDatabase method call(read 3 cars from file and store in cars array of objects)
+        carsDatabase(cars, input2);
 
-    public void setCity(String city) {
-        this.city = city;
-    }
+        
+        //login choices
+        System.out.println("----------------------------------------------------");
+        System.out.println("              MYRENTAL              " + "\n enter 1 to log in and 2 to sign up");
+        System.out.println("----------------------------------------------------");
+        int choice = input1.nextInt();
 
-    public void setCar(Car car) {
-        this.car = car;
-    }
+        if (choice == 1) {
+            //if the user chooses to login
+           cust = new Customer();
+            cust.login();
+        } else if (choice == 2) {
+            //if the user chooses to suign up, add new user to the 4th empty slot
+            Customer newCustomer = new Customer();
+            newCustomer.Signup();
+            customer[4] = newCustomer;
+        } //End of if/else
 
-    public void setCustomer(int custID) {
-        this.custID = custID;
-    }
+        //--------------Home Menu--------------
+        int menuChoice;
 
-    public void setManager(Manager manager) {
-        this.manager = manager;
-    }
+        //Choose service
+        System.out.println();
+        System.out.println("----------------------------------------------------");
+        System.out.println("Please enter your choice :");
+        System.out.println("1-view profile\n"
+                + "2-make rental\n"
+                + "3-edit current rentals\n"
+                + "4-rental history");
+        menuChoice = input1.nextInt();
+        System.out.println();
+        
+        //If user enters a wrong choice (number), then repeat
+        while (menuChoice > 4) {
+            System.out.println("wrong choice, choose again");
+            System.out.println("1-view profile\n"
+                    + "2-make rental\n"
+                    + "3-edit current rentals\n"
+                    + "4-rental history");
+            menuChoice = input1.nextInt();
+            System.out.println();
+           
+        } //End of while loop
 
-    public String getStartingdate() {
-        return Startingdate;
-    }
+        //iteration1: Make rental 
+        if (menuChoice == 2) {
+            makeRental(menuChoice);
+            selectDate();
+            chooseCar();
+             Rental rental = new Rental(start, end, region1, branch1, city1, car[carNum], custID);
+            customer[custID].setRental(rental);
+        } //End of if
 
-    public String getEndingDate() {
-        return endingDate;
-    }
+      
+        
+        
+        //iteration 2: Make Payment 
+        customer[custID].makePayment(custID);
+        
+        input.close();
+        input2.close();
 
-    public String getRegion() {
-        return region;
-    }
+    } //End of main method
 
-    public String getCity() {
-        return city;
-    }
+/*
+    public static Customer signUp() {
+        Customer CurrCustomer = new Customer();
+        CurrCustomer.Signup();
+        System.out.println("sign-up succesful ");
+        CurrCustomer.login();//go to login page after sign up
+        return CurrCustomer;
 
-    public Car getCar() {
-        return car;
-    }
+    } //End of signUp method */
 
-    public int getCustomer() {
-        return custID;
-    }
+    public static void usersDatabase(File customers, Scanner input) {
 
-    public Manager getManager() {
-        return manager;
-    }
-    
-    public boolean makeRental(){
-        return true;
-    }
-    
-    public void editRent(){
+        //read 3 users information from file and store in users array of object
+        int i = 0;
+        while (input.hasNext()) {
+            customer[i] = new Customer();
+            customer[i].setID(i);
+            customer[i].setfName(input.next());
+            customer[i].setlName(input.next());
+            customer[i].setUsername(input.next());
+            customer[i].setPassword(input.next());
+            customer[i].setEmail(input.next());
+
+            i++;
+
+        } //End of while loop
+
+        //input.close();
+    } //End of Database method
+
+    public static void carsDatabase(File cars, Scanner input) {
+
+        //read 3 cars information from file and store in cars array of object
+        int i = 0;
+        while (input.hasNext()) {
+            car[i] = new Car();
+            car[i].setCompany(input.next());
+            car[i].setName(input.next());
+            car[i].setYear(input.next());
+            car[i].setType(input.next());
+            car[i].setColor(input.next());
+            car[i].setPlateNum(input.next());
+            car[i].setAvailability(input.nextBoolean());
+            car[i].setPrice(input.nextDouble());
+
+            i++;
+
+        } //End of while loop
+
+        //input.close();
+    } //End of Database method
+
+    public static void makeRental(int menuChoice) {
+
+        int region, city, branch;
+System.out.println("----------------------------------------------------");
+        System.out.println("              Make Rental              " );
+        System.out.println("----------------------------------------------------");
+        //Choose region
+        System.out.println("    Step1: Choose region: ");
+        System.out.println();
+        System.out.println("1-Makkah Region\n"
+                + "2-Riyadh Region");
+        region = input1.nextInt();
+        if (region == 1) {
+            region1 = "Makkah";
+        } else {
+            region1 = "Riyadh";
+        }
+        System.out.println();
+
+        //If user enters a wrong choice (number), then repeat
+        while (region > 2) {
+            System.out.println("wrong choice, choose again");
+            System.out.println("Choose the region:\n"
+                    + "1-Makkah Region\n"
+                    + "2-Riyadh Region");
+            region = input1.nextInt();
+            System.out.println();
+        } //End of while loop
+
+        //Choose city & branch
+        switch (region) {
+            case 1: {
+                //Choose city in Makkah Region
+                System.out.println("    Step2: Choose city: ");
+                System.out.println();
+                System.out.println("1-Makkah\n"
+                        + "2-Jeddah");
+                city = input1.nextInt();
+                System.out.println();
+
+                if (city == 1) {
+                    //Choose branch in Makkah
+                    System.out.println("    Step3: Choose branch: ");
+                    System.out.println();
+                    System.out.println("1-Albawadi branch\n"
+                            + "2-Alharamain branch");
+                    branch = input1.nextInt();
+                    System.out.println();
+                    city1 = "Makkah";
+                    if (branch == 1) {
+                        branch1 = "Albawadi branch";
+                    } else {
+                        branch1 = "Alharamain branch";
+                    }
+
+                } else if (city == 2) {
+                    //Choose branch in Jeddah
+                    System.out.println("    Step3: Choose branch: ");
+                    System.out.println();
+                    System.out.println("1-Alfaihaa branch\n"
+                            + "2-Alnaseem branch");
+                    branch = input1.nextInt();
+                    System.out.println();
+                    city1 = "Jeddah";
+                    if (branch == 1) {
+                        branch1 = "Alfaihaa branch";
+                    } else {
+                        branch1 = "Alnaseem branch";
+                    }
+                } //End of if/else
+
+            }
+            break; //End of case 1 
+
+            case 2: {
+                //Choose city in Riyadh Region
+                System.out.println("    Step2: Choose city: ");
+                System.out.println();
+                System.out.println("1-Riyadh");
+                city = input1.nextInt();
+                System.out.println();
+                city1 = "Riyadh";
+
+                if (city == 1) {
+                    //Choose branch in Riyadh
+                    System.out.println("    Step3: Choose branch: ");
+                    System.out.println();
+                    System.out.println( "1-Alshaalan branch\n"
+                            + "2-Alwadi branch");
+                    branch = input1.nextInt();
+                    System.out.println();
+                    if (branch == 1) {
+                        branch1 = "Alshaalan branch";
+                    } else {
+                        branch1 = "Alwadi branch";
+                    }
+                } //End of if
+            }
+            break; //End of case 2
+
+            default: {
+
+            }
+            break; //End of default case (error)
+
+        } //End of switch case
+
+    } //End of makeRental method
+
+    public static void selectDate() {
+        System.out.println("    Step3: Select rental starting date and ending date: ");
+        System.out.println();
+        System.out.println("Enter starting date: (DD/MM/YYYY)");
+        start = input1.next();
+        System.out.println("Enter ending date: (DD/MM/YYYY)");
+        end = input1.next();
+        System.out.println();
+        
         
     }
-    
-    public void makepayment(){
-        
-    }
-    
-    public void requestService(){
-        
-    }
 
-    @Override
-    public String toString() {
-        return "Rental{" + "Startingdate=" + Startingdate + ", endingDate=" + endingDate + ", region=" + region + ", city=" + city + ", car=" + car + ", customer=" + custID + ", manager=" + manager + '}';
-    }
-    
+    public static void chooseCar() {
+        System.out.println("    Step4: Choose car: ");
+        System.out.println();
+int j=1;
+        for (int i = 0; i < car.length; i++) {
 
-} //End of class
+            System.out.println((j) + "- \n"
+                    + "Model: " + car[i].getCompany() + " " + car[i].getName() + " " + car[i].getYear() + "\n"
+                    + "Type: " + car[i].getType() + "\n"
+                    + "Color: " + car[i].getColor() + "\n"
+                    + "Price: " + car[i].getPrice() + "$"+"\n");
+            j++;
+        } //End of for loop
+        int choice = input1.nextInt();
+        //because the array is starts from zero
+        choice--;
+        carNum = choice;
+       
+    } //End of chooseCar method
+
+}  //End of class
