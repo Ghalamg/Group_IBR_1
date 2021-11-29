826a3b472659fb5e64130588e97e0d4d36fc0d05
sarahFallatah <91352423+sarahFallatah@users.noreply.github.com>
2021-11-29 21:06:22 +0300

    Update services.java

--- services.java
+++ services.java
@@ -1,34 +1,134 @@
 package myrental;
 
+import java.util.Date;
+import static myrental.MyRental.input1;
+import static myrental.MyRental.service;
+
 public class services {
-    private String serviceType;
-    private double price;
 
-    public services(String serviceType, double price) {
-        this.serviceType = serviceType;
-        this.price = price;
+    private boolean delivery;
+    private double deliveryFee;
+    private boolean insuarnce;
+    private double insuranceFee;
+
+    public services() {
     }
 
-    public String getServiceType() {
-        return serviceType;
+    public services(boolean delivery, double deliveryFee, boolean insuarnce, double insuranceFee) {
+        this.delivery = delivery;
+        this.deliveryFee = deliveryFee;
+        this.insuarnce = insuarnce;
+        this.insuranceFee = insuranceFee;
     }
 
-    public double getPrice() {
-        return price;
+    public boolean isDelivery() {
+        return delivery;
     }
 
-    public void setServiceType(String serviceType) {
-        this.serviceType = serviceType;
+    public void setDelivery(boolean delivery) {
+        this.delivery = delivery;
     }
 
-    public void setPrice(double price) {
-        this.price = price;
+    public double getDeliveryFee() {
+        return deliveryFee;
+    }
+
+    public void setDeliveryFee(double deliveryFee) {
+        this.deliveryFee = deliveryFee;
+    }
+
+    public boolean isInsuarnce() {
+        return insuarnce;
+    }
+
+    public void setInsuarnce(boolean insuarnce) {
+        this.insuarnce = insuarnce;
+    }
+
+    public double getInsuranceFee() {
+        return insuranceFee;
+    }
+
+    public void setInsuranceFee(double insuranceFee) {
+        this.insuranceFee = insuranceFee;
+    }
+
+    public void selectService() {
+        String choice;
+        System.out.println("----------------------------------------------------");
+        System.out.println("    Step 5: Select services ");
+        System.out.println();
+        System.out.println("------------ Delivery ------------");
+        System.out.println("Would you like for the car to be delivered to your location? (YES/NO)");
+        choice = input1.next();
+        if (choice.equalsIgnoreCase("yes")) {
+            this.setDelivery(true);
+            this.setDeliveryFee(50);
+        }
+
+        System.out.println();
+        System.out.println("------------ Insurance ------------");
+        System.out.println("Would you like to include insurance on the car? (YES/NO)");
+
+        choice = input1.next();
+        if (choice.equalsIgnoreCase("yes")) {
+            this.setInsuarnce(true);
+            this.setInsuranceFee(500);
+        }
+
+    }
+    
+    public void mainServices() {
+        int choice;
+        System.out.println("     Select services     ");
+        System.out.println("1- insurance\n "
+                + "2- delivery\n"
+                + "repair\n");
+        choice = input1.nextInt();
+        
+
+    }
+
+    public void insurance() {
+        System.out.println("OUR INSURANCE EVELS"
+                + "\n1-LIABILITY COVERAGE"
+                + "\n2-COMPREHENSIVE COVERAGE "
+                + "\n3-COLLISION COVERAGE");
+        //payment
+        System.out.println("service confirmed \nThank you for choosing our services");
+    }
+
+    public void repair() {                         
+        String carModel;
+        String poblem;
+       String date;
+        System.out.println("enter car type/model: ");
+        carModel=input1.next();
+        System.out.println("enter problem decryption: ");
+        poblem=input1.nextLine();
+        System.out.println("enter repair date: ");
+        date=input1.next();
+         //payment
+        System.out.println("service confirmed \nThank you for choosing our services");
+    }
+
+    public void delivery() {
+        String date;
+        String time;
+        String address;
+        System.out.println("enter delivery date: ");
+        date=input1.next();
+         System.out.println("enter delivery time: ");
+         time=input1.next();
+         System.out.println("enter delivery location: ");
+         address=input1.nextLine();
+         //payment
+         System.out.println("service confirmed \nThank you for choosing our services");
     }
 
     @Override
     public String toString() {
-        return "services{" + "serviceType=" + serviceType + ", price=" + price + '}';
+        return "services{" + "delivery=" + delivery + ", deliveryFee=" + deliveryFee + ", insuarnce=" + insuarnce + ", insuranceFee=" + insuranceFee + '}';
     }
-    
-    
+
 } //End of class
